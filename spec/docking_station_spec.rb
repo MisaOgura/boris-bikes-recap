839c3118333bf6528472e85f8dd94ca0d0e6950f
Misa Ogura <misa.ogura01@gmail.com>
2016-03-02 06:02:48 +0000

    Implement dock_bike method (with 1 argument) and show_bike method to DockingStation class.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,37 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it {is_expected.to respond_to :release_bike}
+  # list of methods DockingStation responds to
+  it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :dock_bike }
+  it { is_expected.to respond_to :show_bike }
 
-  it "releases working bikes" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe "#release_bike" do
+
+    it "releases working bikes" do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+  end
+
+  describe "#dock_bike" do
+
+    it "accepts bikes to be docked" do
+      bike = subject.release_bike
+      expect(subject).to respond_to(:dock_bike).with(1).argument
+    end
+
+  end
+
+  describe "#show_bike" do
+
+    it "shows docked bikes" do
+      bike = subject.release_bike
+      subject.dock_bike bike
+      expect(subject.show_bike).to eq(bike)
+    end
+
   end
 
 end
